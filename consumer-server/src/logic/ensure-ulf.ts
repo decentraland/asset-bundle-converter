0a4c5f26cff7195b7d640ff5335545697e96fa86
menduz <github@menduz.com>
2023-02-01 08:41:45 -0300

    extra license check

--- consumer-server/src/logic/ensure-ulf.ts
+++ consumer-server/src/logic/ensure-ulf.ts
@@ -5,12 +5,15 @@ export function ensureUlf() {
   const path = `/root/.local/share/unity3d/Unity/Unity_lic.ulf`
   const hasEnvVar = !!process.env.UNITY_2021_ULF
 
-  if (!fs.existsSync(path)) {
+  if (!fs.existsSync(path) || !fs.statSync(path).size) {
     if (!hasEnvVar) {
       throw new Error('Neither Unity_lic.ulf or env var UNITY_2021_ULF are available')
     } else {
+      console.log(`Writing file ${path}`)
       fs.mkdirSync(dirname(path), { recursive: true })
       fs.writeFileSync(path, process.env.UNITY_2021_ULF!)
     }
+  } else {
+    console.log(`License ${path} exists`)
   }
 }
