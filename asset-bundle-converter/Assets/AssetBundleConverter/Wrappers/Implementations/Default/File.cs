74e27d194a504729e07d8b3b833f9dfc0762142a
Patricio Guerra <kinerius@gmail.com>
2023-05-02 12:02:54 -0300

    Rollback unintended modified file

--- asset-bundle-converter/Assets/AssetBundleConverter/Wrappers/Implementations/Default/File.cs
+++ asset-bundle-converter/Assets/AssetBundleConverter/Wrappers/Implementations/Default/File.cs
@@ -11,60 +11,41 @@ namespace AssetBundleConverter.Wrappers.Implementations.Default
         {
             public void Delete(string path)
             {
-                path = path.ToLowerInvariant();
                 try { System.IO.File.Delete(path); }
                 catch (Exception e) { Debug.LogError($"Error trying to delete file {path}!\n{e.Message}"); }
             }
 
-            public bool Exists(string path)
-            {
-                path = path.ToLowerInvariant();
-                return System.IO.File.Exists(path);
-            }
+            public bool Exists(string path) =>
+                System.IO.File.Exists(path);
 
             public void Copy(string srcPath, string dstPath)
             {
-                srcPath = srcPath.ToLowerInvariant();
-                dstPath = dstPath.ToLowerInvariant();
                 System.IO.File.Copy(srcPath, dstPath);
             }
 
             public void Move(string srcPath, string dstPath)
             {
-                srcPath = srcPath.ToLowerInvariant();
-                dstPath = dstPath.ToLowerInvariant();
                 System.IO.File.Move(srcPath, dstPath);
             }
 
-            public string ReadAllText(string path)
-            {
-                path = path.ToLowerInvariant();
-                return System.IO.File.ReadAllText(path);
-            }
+            public string ReadAllText(string path) =>
+                System.IO.File.ReadAllText(path);
 
             public void WriteAllText(string path, string text)
             {
-                path = path.ToLowerInvariant();
                 System.IO.File.WriteAllText(path, text);
             }
 
             public void WriteAllBytes(string path, byte[] bytes)
             {
-                path = path.ToLowerInvariant();
                 System.IO.File.WriteAllBytes(path, bytes);
             }
 
-            public Stream OpenRead(string path)
-            {
-                path = path.ToLowerInvariant();
-                return System.IO.File.OpenRead(path);
-            }
+            public Stream OpenRead(string path) =>
+                System.IO.File.OpenRead(path);
 
-            public byte[] ReadAllBytes(string texturePath)
-            {
-                texturePath = texturePath.ToLowerInvariant();
-                return System.IO.File.ReadAllBytes(texturePath);
-            }
+            public byte[] ReadAllBytes(string texturePath) =>
+                System.IO.File.ReadAllBytes(texturePath);
         }
     }
 }
