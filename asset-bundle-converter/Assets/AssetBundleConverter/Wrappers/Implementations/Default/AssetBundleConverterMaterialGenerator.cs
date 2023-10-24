cd9a20dfc7b9d2e4cd62dbf1a944ef663438f0a5
Mikhail Agapov <mikhail.agapov@decentraland.org>
2023-10-24 17:28:15 +0200

    Enable keywords for the "DCL/Scene" shader

--- asset-bundle-converter/Assets/AssetBundleConverter/Wrappers/Implementations/Default/AssetBundleConverterMaterialGenerator.cs
+++ asset-bundle-converter/Assets/AssetBundleConverter/Wrappers/Implementations/Default/AssetBundleConverterMaterialGenerator.cs
@@ -1,18 +1,37 @@
 ﻿using DCL.GLTFast.Wrappers;
+using GLTFast;
+using UnityEngine;
 
 namespace AssetBundleConverter.Wrappers.Implementations.Default
 {
     public class AssetBundleConverterMaterialGenerator : DecentralandMaterialGenerator
     {
+        private static bool useSceneShader =>
+#if UNITY_WEBGL
+            false;
+#else
+            true;
+#endif
+
         public AssetBundleConverterMaterialGenerator() : base(GetShaderName()) { }
 
-        private static string GetShaderName()
+        private static string GetShaderName() =>
+            useSceneShader ? "DCL/Scene" : "DCL/Universal Render Pipeline/Lit";
+
+        public override Material GenerateMaterial(int materialIndex, GLTFast.Schema.Material gltfMaterial, IGltfReadable gltf, bool pointsSupport = false)
         {
-            #if UNITY_WEBGL
-            return "DCL/Universal Render Pipeline/Lit";
-            #else
-            return "DCL/Scene";
-            #endif
+            var mat = base.GenerateMaterial(materialIndex, gltfMaterial, gltf, pointsSupport);
+
+            if (useSceneShader)
+            {
+                // Enable Forward+ and soft shadows
+                mat.EnableKeyword("_FORWARD_PLUS");
+                mat.EnableKeyword("_ADDITIONAL_LIGHT_SHADOWS");
+                mat.EnableKeyword("_MAIN_LIGHT_SHADOWS_CASCADE");
+                mat.EnableKeyword("_SHADOWS_SOFT");
+            }
+
+            return mat;
         }
     }
 }
