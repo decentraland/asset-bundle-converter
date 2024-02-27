f0222968ea3972be28cf2b04976733fa17d8f91a
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-02-27 15:33:17 -0300

    feat: add runLodsConversion function

--- consumer-server/src/logic/run-conversion.ts
+++ consumer-server/src/logic/run-conversion.ts
@@ -5,6 +5,70 @@ import { dirname } from 'path'
 import { AppComponents } from '../types'
 import { execCommand } from './run-command'
 
+async function makeLogFileAndOutputDirectoryAvailable(options: {
+  logFile: string,
+  outDirectory: string
+}) {
+  // touch logfile and create folders
+  await fs.mkdir(dirname(options.logFile), { recursive: true })
+  await fs.mkdir(options.outDirectory, { recursive: true })
+  closeSync(openSync(options.logFile, 'w'))
+}
+
+async function executeProgram(options: { logger: ILoggerComponent.ILogger, components: Pick<AppComponents, 'metrics'>, childArg0: string, childArguments: string[], projectPath: string, timeout: number }) {
+  const { logger, components, childArg0, childArguments, projectPath, timeout } = options
+  const { exitPromise, child } = execCommand(logger, childArg0, childArguments, process.env as any, projectPath)
+
+  if (timeout) {
+    setTimeout(() => {
+      if (exitPromise.isPending) {
+        try {
+          if (!child.killed) {
+            logger.warn('Process did not finish', { pid: child.pid?.toString() || '?', command: childArg0, args: childArguments.join(' ') } as any)
+            components.metrics.increment('ab_converter_timeout')
+            exitPromise.reject(new Error('Process did not finish'))
+            if (!child.kill('SIGKILL')) {
+              logger.error('Error trying to kill child process', { pid: child.pid?.toString() || '?', command: childArg0, args: childArguments.join(' ') } as any)
+            }
+          }
+        } catch (err: any) {
+          logger.error(err)
+        }
+      }
+    }, timeout)
+  }
+
+  return await exitPromise
+}
+
+export async function runLodsConversion(logger: ILoggerComponent.ILogger, components: Pick<AppComponents, 'metrics'>, options: {
+  logFile: string,
+  outDirectory: string,
+  entityId: string,
+  lodBucket: string,
+  lodBucketDirectory: string,
+  unityPath: string,
+  projectPath: string,
+  timeout: number
+}) {
+  makeLogFileAndOutputDirectoryAvailable(options)
+
+  const childArg0 = `${options.unityPath}/Editor/Unity`
+
+  const childArguments: string[] = [
+    '-projectPath', options.projectPath,
+    '-batchmode',
+    '-executeMethod', 'DCL.ABConverter.SceneClient.ExportFBXAssetBundles',
+    '-sceneCid', options.entityId,
+    '-logFile', options.logFile,
+    '-bucket', options.lodBucket,
+    '-bucketDirectory', options.lodBucketDirectory,
+    '-output', options.outDirectory
+  ]
+
+  return await executeProgram({ logger, components, childArg0, childArguments, projectPath: options.projectPath, timeout: options.timeout })
+}
+
 export async function runConversion(
   logger: ILoggerComponent.ILogger,
   components: Pick<AppComponents, 'metrics'>,
@@ -19,10 +83,7 @@ export async function runConversion(
     unityBuildTarget: string,
   }
 ) {
-  // touch logfile and create folders
-  await fs.mkdir(dirname(options.logFile), { recursive: true })
-  await fs.mkdir(options.outDirectory, { recursive: true })
-  closeSync(openSync(options.logFile, 'w'))
+  makeLogFileAndOutputDirectoryAvailable(options)
 
   // normalize content server URL
   let contentServerUrl = options.contentServerUrl
@@ -46,26 +107,5 @@ export async function runConversion(
     '-buildTarget', options.unityBuildTarget
   ]
 
-  const { exitPromise, child } = execCommand(logger, childArg0, childArguments, process.env as any, options.projectPath)
-
-  if (options.timeout) {
-    setTimeout(() => {
-      if (exitPromise.isPending) {
-        try {
-          if (!child.killed) {
-            logger.warn('Process did not finish', { pid: child.pid?.toString() || '?', command: childArg0, args: childArguments.join(' ') } as any)
-            components.metrics.increment('ab_converter_timeout')
-            exitPromise.reject(new Error('Process did not finish'))
-            if (!child.kill('SIGKILL')) {
-              logger.error('Error trying to kill child process', { pid: child.pid?.toString() || '?', command: childArg0, args: childArguments.join(' ') } as any)
-            }
-          }
-        } catch (err: any) {
-          logger.error(err)
-        }
-      }
-    }, options.timeout)
-  }
-
-  return await exitPromise
+  return await executeProgram({ logger, components, childArg0, childArguments, projectPath: options.projectPath, timeout: options.timeout })
 }
