817b783f7b8cbe60af612fc4fa5678c3fcb8f235
Mariano Goldman <marianogoldman@users.noreply.github.com>
2024-08-14 17:06:46 -0300

    chore: use node 18 (#113)

--- consumer-server/src/controllers/handlers/queue-conversion-handle.ts
+++ consumer-server/src/controllers/handlers/queue-conversion-handle.ts
@@ -2,9 +2,8 @@ import { HandlerContextWithPath } from '../../types'
 import { DeploymentToSqs } from '@dcl/schemas/dist/misc/deployments-to-sqs'
 import { IHttpServerComponent } from '@well-known-components/interfaces'
 
-// handlers arguments only type what they need, to make unit testing easier
 export async function queueTaskHandler(
-  context: HandlerContextWithPath<'metrics' | 'taskQueue' | 'config', '/queue-task'>
+  context: HandlerContextWithPath<'taskQueue' | 'config', '/queue-task'>
 ): Promise<IHttpServerComponent.IResponse> {
   const {
     components: { taskQueue, config },
