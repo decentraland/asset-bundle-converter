95a9c9491f9799f5af88acddb22e7ff3544f6fe5
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2025-01-17 17:36:53 -0300

    chore: add logs when publishing events to SNS (#191)

--- consumer-server/src/adapters/sns.ts
+++ consumer-server/src/adapters/sns.ts
@@ -2,7 +2,11 @@ import { PublishCommand, SNSClient } from '@aws-sdk/client-sns'
 import { AppComponents, PublisherComponent } from '../types'
 import { AssetBundleConversionFinishedEvent, AssetBundleConversionManuallyQueuedEvent } from '@dcl/schemas'
 
-export async function createSnsComponent({ config }: Pick<AppComponents, 'config'>): Promise<PublisherComponent> {
+export async function createSnsComponent({
+  config,
+  logs
+}: Pick<AppComponents, 'config' | 'logs'>): Promise<PublisherComponent> {
+  const logger = logs.getLogger('publisher')
   const snsArn = await config.requireString('AWS_SNS_ARN')
   const optionalEndpoint = await config.getString('AWS_SNS_ENDPOINT')
 
@@ -29,6 +33,9 @@ export async function createSnsComponent({ config }: Pick<AppComponents, 'config
     })
 
     await client.send(command)
+    logger.info('Published message to SNS', {
+      entityId: event.metadata.entityId
+    })
   }
 
   return { publishMessage }
