b91ea594eb873af5ad2e80e115e1ab204db23703
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-08-05 11:39:19 +0300

    style: Fix linter

--- consumer-server/src/utils.ts
+++ consumer-server/src/utils.ts
@@ -1,11 +1,11 @@
 export function getUnityBuildTarget(target: string): string | undefined {
   switch (target) {
-    case "webgl":
-      return "WebGL"
-    case "windows":
-      return "StandaloneWindows64"
-    case "mac":
-      return "StandaloneOSX"
+    case 'webgl':
+      return 'WebGL'
+    case 'windows':
+      return 'StandaloneWindows64'
+    case 'mac':
+      return 'StandaloneOSX'
     default:
       return undefined
   }
@@ -13,13 +13,13 @@ export function getUnityBuildTarget(target: string): string | undefined {
 
 export function getAbVersionEnvName(buildTarget: string) {
   switch (buildTarget) {
-    case "webgl":
-      return "AB_VERSION"
-    case "windows":
-      return "AB_VERSION_WINDOWS"
-    case "mac":
-      return "AB_VERSION_MAC"
+    case 'webgl':
+      return 'AB_VERSION'
+    case 'windows':
+      return 'AB_VERSION_WINDOWS'
+    case 'mac':
+      return 'AB_VERSION_MAC'
     default:
-      throw "Invalid buildTarget"
+      throw 'Invalid buildTarget'
   }
 }
