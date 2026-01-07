6d9a3c39ae8a05389b132ce40b392462f8169574
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2026-01-07 08:37:16 -0300

    chore: update ISS ab version
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- consumer-server/src/service.ts
+++ consumer-server/src/service.ts
@@ -51,7 +51,7 @@ export async function main(program: Lifecycle.EntryPointParameters<AppComponents
           // Increment version if doISS is true
           let versionToUse = $AB_VERSION
           if (job.doISS) {
-            versionToUse = 'v2001'
+            versionToUse = 'v2002'
           }
 
           if (job.lods) {
